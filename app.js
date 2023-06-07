ef98ba354583ad6c9c48d800922293c3749d7ac5
Ebad-Uddin <ebad0300@gmail.com>
2023-06-07 14:16:44 +0500

    search functionality added with js

--- app.js
+++ app.js
@@ -1,97 +1,70 @@
-//task-2 
+// (function () {
+//   fetch("https://dummyjson.com/products")
+//     .then((response) => response.json())
+//     .then((data) => {
+//       console.log(data);
+//       let rootElem = document.getElementById("rootElem");
+//       data.products.forEach((e) => {
+//         // console.log(e.title);
+//         rootElem.innerHTML += `
+//       <div class="col-lg-3 col-md-4 col-sm-6">
+//         <div class="card">
+//             <img src="${
+//               e.images[0]
+//             }" class="card-img-top" alt="..." height="200px">
+//             <div class="card-body">
+//               <h5 class="card-title">${e.title + e.brand}</h5>
+//               <p class="card-text">${e.description}</p>
+//               <a href="#" class="btn btn-primary">${e.price}</a>
+//             </div>
+//           </div>
 
+//     </div>
+//       `;
+//       });
+//     });
+// })();
 
-// for(let i = 0; i <= 100; i++){
-//     console.log(i)
-//     if(i % 3 === 0){
-//         console.log("Fizz");
-//     }
-//     else if(i % 5 === 0){
-//         console.log("Buzz");
-//     }
-//     else if(i % 3 === 0 && i % 5 === 0 ){
-//         console.log("FizzBuzz");
-//     }
-//     else{
-//         console.log("Error");
-//     }
-// }
+(async function () {
+  await fetch("json/recipes.json")
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
 
+      let searchBar = document.getElementById("searchBar");
+      let btn = document.getElementById("btn");
+      let list = document.getElementById("myList");
 
-// let randNum = Math.round(Math.random() * 100);
-// if(randNum % 3 === 0){
-//             console.log("Fizz");
-//         }
-//         else if(randNum % 5 === 0){
-//             console.log("Buzz");
-//         }
-//         else if(randNum % 3 === 0 && randNum % 5 === 0 ){
-//             console.log("FizzBuzz");
-//         }
+      function displayResult(result) {
+        list.innerHTML = "";
+        //  console.log(result)
+        result.forEach((value) => {
+          list.innerHTML += `
+  <li>
+  <h2>${value.title}</h2>
+  <p>${value.description}</p>
+  
+  </li>
+  
+  
+  `;
+        });
+      }
 
+      function search() {
+        let query = searchBar.value;
+        console.log(query);
 
-// console.log(randNum) 
+        let result = data.filter(function (recipe) {
+          return (
+            recipe.title.toLowerCase().includes(query) ||
+            recipe.ingredients.join("").toLowerCase().includes(query)
+          );
+        });
+        // console.log(result)
+        displayResult(result);
+      }
 
-
-
-// //Task-4
-
-// // Task-4
-
-// let arr = ["v", "c", "u", "y", "t", "e"];
-
-// // let sorting = ;
-// console.log(arr.sort());
-
-// Immediately invoke function
-
-(async function (){
-await fetch('/json/recipes.json').then(response => response.json()).then(data => {
-    console.log(data)
-    let container = document.getElementById('container');
-    let rootElem = document.getElementById('rootElem');
-    for(let i = 0; i < data.length; i++){
-        // console.log(data[i])
-        rootElem.innerHTML += `
-        <div class="card">
-    <div class="card-body">
-    <h5 class="card-title"> ${data[i].title} </h5>
-    <p class="card-text"> ${data[i].ingredients}</p>
-
-  </div>
-</div>
-     `
-
-    }
-
-
-
-})  
-
-})()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+      btn.addEventListener("click", search);
+    });
+})();
